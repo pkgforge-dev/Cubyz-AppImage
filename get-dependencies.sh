ae84a8ba3d7fd407484f2b1586905b38d1697242
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-04-24 04:08:39 -0400

    get app

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,10 +10,10 @@ echo "---------------------------------------------------------------"
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano ! gtk ! qt
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package cubyz-bin
 
 # If the application needs to be manually built that has to be done down here
 
