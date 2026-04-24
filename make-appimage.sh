b2c748391f289499df9a64df09c8df175a15172c
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-04-24 04:15:18 -0400

    deploy app

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,21 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q cubyz | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/pixmaps/cubyz.png
+export DESKTOP=/usr/share/applications/cubyz.desktop
+export DEPLOY_OPENGL=1
+export DEPLOY_VULKAN=1
+export DEPLOY_SDL=1 # SDL is statically linked, we still need to do this to cover dlopened libraries
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+mkdir -p ./AppDir/bin
+cp -rv /opt/cubyz/* ./AppDir/bin
+quick-sharun ./AppDir/bin/*
 
 # Additional changes can be done in between here
 
